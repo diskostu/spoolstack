e8fe4b9909eeebbdb40fbef8eae68b9414050bc4
Denny Kluge <denny.kluge@gmail.com>
2025-11-02 16:54:33 +0100

    refactor: Externalize strings

--- app/src/main/java/de/diskostu/spoolstack/MainActivity.kt
+++ app/src/main/java/de/diskostu/spoolstack/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import de.diskostu.spoolstack.ui.theme.SpoolstackTheme
 
@@ -39,7 +40,7 @@ fun MainScreen(modifier: Modifier = Modifier) {
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Button(onClick = { /* no-op */ }) {
-            Text("Filament hinzufügen")
+            Text(stringResource(R.string.add_filament))
         }
     }
 }
