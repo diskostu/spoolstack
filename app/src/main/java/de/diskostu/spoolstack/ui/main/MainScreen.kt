11a8d8ce2266b331b8a26a59be9553846d34312e
Denny Kluge <denny.kluge@gmail.com>
2025-11-02 18:17:54 +0100

    refactor: Extract MainScreen to its own file

---
+++ app/src/main/java/de/diskostu/spoolstack/ui/main/MainScreen.kt
@@ -0,0 +1,45 @@
+package de.diskostu.spoolstack.ui.main
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import de.diskostu.spoolstack.R
+import de.diskostu.spoolstack.ui.theme.SpoolstackTheme
+
+@Composable
+fun MainScreen(navController: NavController, modifier: Modifier = Modifier) {
+    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+        Column(
+            modifier = modifier
+                .fillMaxSize()
+                .padding(innerPadding),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            // opens the screen to add a new filament
+            Button(onClick = { navController.navigate("add_filament") }) {
+                Text(stringResource(R.string.add_filament))
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MainScreenPreview() {
+    SpoolstackTheme {
+        // Note: Preview doesn't show navigation
+        MainScreen(navController = rememberNavController())
+    }
+}
