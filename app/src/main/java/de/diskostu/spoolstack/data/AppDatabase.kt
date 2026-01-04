389f5df7831f4741e42e7803aff52adeed41ebed
Denny Kluge <denny.kluge@gmail.com>
2026-01-04 21:33:52 +0100

    chore: Disable schema export for AppDatabase

--- app/src/main/java/de/diskostu/spoolstack/data/AppDatabase.kt
+++ app/src/main/java/de/diskostu/spoolstack/data/AppDatabase.kt
@@ -3,7 +3,7 @@ package de.diskostu.spoolstack.data
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [Filament::class], version = 1)
+@Database(entities = [Filament::class], version = 1, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun filamentDao(): FilamentDao
 }
