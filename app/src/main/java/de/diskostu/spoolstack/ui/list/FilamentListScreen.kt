7354742d61b8b876a991a46827c60f4f9251467a
Denny Kluge <denny.kluge@gmail.com>
2026-01-05 23:42:01 +0100

    refactor: Update hiltViewModel import to hilt.lifecycle.viewmodel.compose

--- app/src/main/java/de/diskostu/spoolstack/ui/list/FilamentListScreen.kt
+++ app/src/main/java/de/diskostu/spoolstack/ui/list/FilamentListScreen.kt
@@ -27,7 +27,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import de.diskostu.spoolstack.R
 import de.diskostu.spoolstack.data.Filament
 import de.diskostu.spoolstack.ui.theme.SpoolstackTheme
